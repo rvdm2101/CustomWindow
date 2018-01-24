6b5b3f01f5674c23b7a50115a45e1c9a1c138450
rvdm2101 <35774287+rvdm2101@users.noreply.github.com>
2018-01-24 20:01:14 +0100

    Add files via upload

---
+++ CustomWindow/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management;
+using System.Windows.Interop;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace CustomWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CloseButton.Click += (s, e) => Close();
+            MaximizeButton.Click += (s, e) => WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+            MinimizeButton.Click += (s, e) => WindowState = WindowState.Minimized;
+            SourceInitialized += (s, e) =>
+            {
+                IntPtr handle = (new WindowInteropHelper(this)).Handle;
+                HwndSource.FromHwnd(handle).AddHook(new HwndSourceHook(WindowCursorLocation.WindowProc));
+            };
+        }
+    }
+}
